1243c61cce68043673f6b5f59db904a3b07c9e72
Oliver Lonsdale <oliver.lonsdale@fourquant.com>
2025-03-01 00:01:20 +0000

    Add project files.

---
+++ NameUpdater/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols.Configuration;
+using NameUpdater.Components;
+
+namespace NameUpdater;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidConfigurationException("Connection string is empty");
+
+        builder.Services.AddTransient<SqlConnection>(_ => new SqlConnection(connectionString));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+
+
+
+        app.Run();
+    }
+}
